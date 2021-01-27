a934994062af6a2f12436df43f69d1cd9339e744
Afonso Ramos <afonso.ramos@POLT0058.local>
2021-01-27 11:36:24 +0000

    deleteJob tests rename

--- tests/delete.js
+++ tests/delete.js
@@ -5,7 +5,7 @@
 var CronJobManager = require('../lib/crontab_manager');
 exports.test = () => {
     let date = new Date();
-    crontab1 = new CronJobManager('deleteMe', date, () =>{console.log("delete me...")} );
-    crontab1.deleteJob('deleteMe');
+    crontab1 = new CronJobManager('deleteMe', date, () =>{ console.log("delete me...")} );
+    crontab1.delete('deleteMe');
     console.assert(! crontab1.jobs.deleteMe, "The deleteMe job was not deleted!!! %s", crontab1);
 }
