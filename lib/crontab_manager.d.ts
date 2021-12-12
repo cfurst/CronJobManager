179087555a6a494b14563bc93bbd6626c6e4d891
Carl Yamamoto-Furst <carl.furst@seefurst.com>
2021-12-12 13:51:56 -0500

    added Tab and Option types.

--- lib/crontab_manager.d.ts
+++ lib/crontab_manager.d.ts
@@ -1,7 +1,7 @@
 export = CrontabManager;
 declare class CrontabManager {
-    constructor(key: string, tab: string, task: () => void, options: any)
-    add(key: string, tab: string, task: () => void, options: any): void
+    constructor(key: string, tab: CrontabTypes.Tab, task: () => void, options: CrontabTypes.Options)
+    add(key: string, tab: CrontabTypes.Tab, task: () => void, options: CrontabTypes.Options): void
     update(...args: any): void
     deleteJob(key: string): void
     start(key: string): void
@@ -10,4 +10,19 @@ declare class CrontabManager {
     toString(): string
     listCrons(): string
     exists(tabKey: string): boolean
+}
+
+declare namespace CrontabTypes {
+    export type Tab = string | Date
+    export interface Options {
+        cronTime?: Tab
+        onTick?: () => void
+        onComplete?: () => void
+        start?: boolean
+        timezone?: string
+        context?: any
+        runOnInit?: boolean
+        utcOffset?: string
+        unrefTimeout?: boolean
+    }
 }
