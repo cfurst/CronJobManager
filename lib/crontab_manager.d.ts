b1e290992643683a11be352cd91d48f7d5ae035f
Carl Yamamoto-Furst <carl.furst@seefurst.com>
2021-12-12 13:10:56 -0500

    first working version of types file.

--- lib/crontab_manager.d.ts
+++ lib/crontab_manager.d.ts
@@ -1,19 +1,8 @@
-export = CrontabManager
-declare interface CrontabOptions {
-    cronTime: string,
-    onTick: (): void,
-    onComplete?: (): void,
-    start?: boolean,
-    timeZone?: string,
-    context?: any,
-    runOnInit?: boolean,
-    utcOffset?: string,
-    unrefTimeout: boolean
-}
+export = CrontabManager;
 declare class CrontabManager {
-    constructor(key: string, tab: string, task: () => void, options: CrontabOptions)
-    add(key: string, tab: string | Date, task: () => void, options: CrontabOptions): void
-    update(key: string, arg1: any): void // arg1 can be a cron expression as a string, or a Date object to update the timing. It can be a function to update the task.
+    constructor(key: string, tab: string, task: () => void, options: any)
+    add(key: string, tab: string, task: () => void, options: any): void
+    update(...args: any): void
     deleteJob(key: string): void
     start(key: string): void
     stop(key: string): void
