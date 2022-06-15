6875ea7357cc86ae8eb771b375b99d8e2a9933c4
Carl Yamamoto-Furst <carl.furst@seefurst.com>
2022-06-15 11:18:07 -0400

    add back the original update tests

--- tests/update.js
+++ tests/update.js
@@ -5,6 +5,39 @@ const CronJobManager = require('../lib/crontab_manager'),
 cronTab = new CronJobManager();
 
 exports.test = () => {
+  let currDate = new Date();
+  cronTab.add('updateTest', currDate, () => {console.log("update Cron... wooo!")});
+
+  //update the tab - string
+  let tabString = '0 */2 * * * *';
+  cronTab.update('updateTest', tabString);
+  console.assert(/0 \*\/2 \* \* \* \*/.test(cronTab), `Could not find updated crontab String! ${cronTab}`);
+
+  //update the tab - Date;
+  let testDate = new Date('2018-08-24T09:57:22-0400');
+  cronTab.update('updateTest', testDate);
+  console.assert(/Fri Aug 24 2018 09\:57\:22 GMT-0400/.test(cronTab), `Could not find updated Crontab date! ${cronTab}`);
+
+  //update the task
+  let newFunc = function() {
+
+      console.log("you can now doo a double wooo... wooo wooo....");
+  }
+
+  cronTab.update('updateTest', newFunc);
+  console.assert(`${cronTab}`.search("double wooo"), "coulnd't the update task when updating just the task!");
+
+  //update the tab and the task - we know the update tab works. We just need to make sure the task gets updated.
+  cronTab.update('updateTest', testDate, () => {console.log("A New Task!")});
+
+  console.assert(`${cronTab}`.search("A New Task!"), "could't find the updated task when updating tab and task!");
+
+  let thirtyOne = 31
+  cronTab.add(thirtyOne.toString(),new Date(), () => {console.log("new Job...")})
+  cronTab.update(thirtyOne.toString(), new Date(), () => {console.log("updated 31...")})
+
+  console.assert(`${cronTab}`.search("updated 31..."), "couldn't the update task when updating a task with a number literal coerced to string.");
+
    let additionalOptions = {utcOffset: "-5", onComplete: () => {console.log("I'm done working")},start: true}
    
    function testAdditionalOptions() {
