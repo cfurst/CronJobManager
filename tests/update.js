9ccb4c424629c7b0fd7f6b455ad952f2d697036c
Carl Yamamoto-Furst <carl.furst@seefurst.com>
2022-06-15 10:40:14 -0400

    test update tab as well

--- tests/update.js
+++ tests/update.js
@@ -6,17 +6,23 @@ cronTab = new CronJobManager();
 
 exports.test = () => {
    let additionalOptions = {utcOffset: "-5", onComplete: () => {console.log("I'm done working")},start: true}
+   
+   function testAdditionalOptions() {
+    [
+      "cronTime",
+      "context",
+      "onComplete",
+     "unrefTimeout"
+    ].forEach( (key) => {
+                                 if (key === "cronTime" && cronTab.jobs["test update with additional"][key] !== undefined && cronTab.jobs["test update with additional"][key].hasOwnProperty('utcOffset')) console.assert(cronTab.jobs["test update with additional"][key].utcOffset === additionalOptions.utcOffset, `${key} did not pass jobs value: ${cronTab.jobs["test update with additional"][key].utcOffset} additionalOptions: ${additionalOptions.utcOffset}`)
+                                 else if (cronTab.jobs["test update with additional"].hasOwnProperty(key) && additionalOptions.hasOwnProperty(key)) console.assert(cronTab.jobs["test update with additional"][key] === additionalOptions[key], `${key} did not pass: jobs value: ${cronTab.jobs["test update with additional"][key]}, additionalOptions valud: ${additionalOptions[key]}`)
+                               })
+   }
    cronTab.add("test update with additional", "* * * * * *", () => {console.log("shhh.. I'm working!")}, additionalOptions )
    cronTab.update("test update with additional", () => {console.log("shh... I'm still working!")});
-   [
-     "cronTime",
-     "context",
-     "onComplete",
-    "unrefTimeout"
-   ].forEach( (key) => {
-                              console.log(key)
-                                if (key === "cronTime" && cronTab.jobs["test update with additional"][key] !== undefined && cronTab.jobs["test update with additional"][key].hasOwnProperty('utcOffset')) console.assert(cronTab.jobs["test update with additional"][key].utcOffset === additionalOptions.utcOffset, `${key} did not pass jobs value: ${cronTab.jobs["test update with additional"][key].utcOffset} additionalOptions: ${additionalOptions.utcOffset}`)
-                                else if (cronTab.jobs["test update with additional"].hasOwnProperty(key) && additionalOptions.hasOwnProperty(key)) console.assert(cronTab.jobs["test update with additional"][key] === additionalOptions[key], `${key} did not pass: jobs value: ${cronTab.jobs["test update with additional"][key]}, additionalOptions valud: ${additionalOptions[key]}`)
-                              })
+   testAdditionalOptions()
+   cronTab.update("test update with additional", "*/2 * * * * *")
+   testAdditionalOptions()
+   
   
 }; 
